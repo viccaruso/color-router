6806080910e31a732a36872920828f1796755b0c
Victor Caruso <vic.caruso@gmail.com>
2022-05-02 15:10:27 -0700

    Get RGB from params

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,16 @@ import styles from './App.css';
 
 function RGB() {
   // 🚨 Get values for r, g, and b from params
+  const { r, g, b } = useParams();
   // 🚨 Uncomment next lines after importing
-  // return (
-  // <div
-  //   className={styles.fill}
-  //   style={{ background: `rgb(${r}, ${g}, ${b})` }}
-  // >
-  //   <p>{`rgb(${r},${g},${b})`}</p>
-  // </div>
-  // );
+  return (
+    <div
+      className={styles.fill}
+      style={{ background: `rgb(${r}, ${g}, ${b})` }}
+    >
+      <p>{`rgb(${r},${g},${b})`}</p>
+    </div>
+  );
 }
 
 function ScreenColor() {
